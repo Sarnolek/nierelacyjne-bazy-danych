4dc99a6f5ec0dc3a21d65d35c65a4c6cd66d7788
kdawid <karol.dawid27@gmail.com>
2025-11-01 15:32:05 +0100

    repositories

---
+++ CarRental/src/main/java/repository/InMemoryClientRepository.java
@@ -0,0 +1,30 @@
+package repository;
+
+import model.Client;
+
+import java.util.*;
+
+public class InMemoryClientRepository implements ClientRepository{
+    private final HashMap<UUID, Client> clients = new HashMap<>();
+
+    @Override
+    public Client save(Client client) {
+        clients.put(client.getId(), client);
+        return client;
+    }
+
+    @Override
+    public Optional<Client> findById(UUID id) {
+        return Optional.ofNullable(clients.get(id));
+    }
+
+    @Override
+    public List<Client> findAll() {
+        return new ArrayList<>(clients.values());
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        clients.remove(id);
+    }
+}
